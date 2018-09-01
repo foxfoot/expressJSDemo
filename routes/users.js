5605c0ff9d0e8eda4542a91d694bc66847bf9fb8
foxfoot <xinyan.zhang@gmail.com>
2018-08-31 22:03:12 -0400

    Pass the 'res' object in the callback function directly

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,7 @@ router.get('/', function(req, res, next) {
   res.send(JSON.stringify(obj, null, 2));
 });
 
-function readFile(fileName, callback){
+function readFile1(fileName, callback){
   fs.access(fileName, err => {
     if(err){
       console.log('error when reading file ' +fileName + " error is: " + err);
@@ -50,27 +50,60 @@ function readFile(fileName, callback){
   })
 }
 
+function readFile2(fileName, res){
+  fs.access(fileName, err => {
+    if(err){
+      console.log('error when reading file ' +fileName + " error is: " + err);
+      res.status(500);
+      res.json({error : err})
+      return;
+    }
+
+    var data = '';
+    var readStream = fs.createReadStream(fileName);
+    readStream.setEncoding('UTF8')
+
+    readStream.on('data', chunk => {
+      data += chunk; 
+      console.log('get a chunk: ' + chunk)
+    })
+
+    readStream.on('end', ()=> {
+      console.log('end');
+      
+      res.json(JSON.parse(data));
+    })
+
+    readStream.on('error', err => {
+      console.log('get an error: ' + err)
+      res.status(500);
+      res.json({error : err})
+    })
+
+  })
+}
+
 router.get('/getUserInfo', (req, res, next) => {
   var user = new User();
   var params = URL.parse(req.url, true).query;
 
-  var fileName = 'data/';
-  console.log('param', params);
-  if(params.id === '1'){
-    fileName += 'Tom.json';
-  } else {
-    fileName += 'Jerry.json';
-  }
+  var fileName = params.fileName || 'Tom';
+  var fullFileName = 'data/' + fileName + '.json';
+  console.log('param', fullFileName);
 
-  readFile(fileName, function(e, data){
+/*  readFile1(fullFileName, function(e, data){  //CB as argument
     if(e){
       console.log('error calling the readFile: ' + e);
+      res.status(500);
+      res.json({error : e})
     }else{
       console.log('The result of calling the readFile: ' + data);
       user = JSON.parse(data);
       res.json(user);  //from Express 3.x
     }
   })
+*/
+  readFile2(fullFileName, res);  // res as argument
 
   //old way. write the response by self -- including head and body
   //res.writeHead(200, {"Content-Type": "application/json"});
